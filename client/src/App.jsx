63fa14c277c86e6f962d4d000747aec3d50bfcd8
Ratan Deep Singh <ratandeep.blr.12@gmail.com>
2025-04-01 23:06:06 +0530

    feat: replace Instagram component with Home component in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { useAuth } from "./hooks/useAuth";
 import { PrimeReactProvider } from "primereact/api";
 import DesignSystem from "./designSystem";
 import { useModalActions } from "./store/useModal";
-// import Home from "./screens/home/Home.jsx";
+import Home from "./screens/home/Home.jsx";
 // import AuthModal from "./screens/AuthModal.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -26,7 +26,7 @@ function App() {
     <PrimeReactProvider value={{ unstyled: true, pt: DesignSystem }}>
       <QueryClientProvider client={queryClient}>
         <AppContainer>
-          <Instagram />
+          <Home />
           {/* {authenticated && <Home />} */}
           {/* <AuthModal /> */}
           <Toaster />
